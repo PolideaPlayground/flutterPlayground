0a1e05b6530dd31fd4ca47b9363e47ebb66d0c99
Pawel Byszewski <>
2019-03-07 13:57:02 +0100

    refactor

--- test/devices_list/devices_bloc_picking_device.dart
+++ test/devices_list/devices_bloc_picking_device.dart
@@ -12,13 +12,13 @@ void main() {
   FlutterBlueMock flutterBlueMock;
   DeviceRepositoryMock deviceRepositoryMock;
   DevicesBloc devicesBloc;
-  BleDevice bleDevice1;
+  BleDevice bleDevice;
 
 
   setUp(() {
     flutterBlueMock = FlutterBlueMock();
     deviceRepositoryMock = DeviceRepositoryMock();
-    bleDevice1 = BleDeviceFactory.buildDisconnected(bluetoothDevice: BluetoothDeviceFactory.build(name: "name1", id: "id1"));
+    bleDevice = BleDeviceFactory.buildDisconnected(bluetoothDevice: BluetoothDeviceFactory.build(name: "name1", id: "id1"));
     devicesBloc = DevicesBloc(flutterBlueMock, deviceRepositoryMock);
 
     when(flutterBlueMock.scan()).thenAnswer((_) => Observable.never());
@@ -29,8 +29,8 @@ void main() {
     devicesBloc.init();
 
     //when
-    devicesBloc.devicePicker.add(bleDevice1);
+    devicesBloc.devicePicker.add(bleDevice);
 
-    await untilCalled(deviceRepositoryMock.pickDevice(argThat(equals(bleDevice1))));
+    await untilCalled(deviceRepositoryMock.pickDevice(argThat(equals(bleDevice))));
   });
 }
